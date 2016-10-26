38013923bdb106fdbeb862f69b316bd076ac0b12
Alex <ajshort2010@hotmail.com>
2016-10-25 23:25:36 -0700

    completed app.js constructor functions will come next.

--- app.js
+++ app.js
@@ -19,29 +19,191 @@ These numbers are simply Pat's estimates for now, but eventually, once there has
 //HTML Generation
 // The HTML currently does not have the body, h1, ul, or li tags we need. We must create them inside JS.
 // To do this we must Document.create tag the body, h1, ul, and li. Fill each one with data as we go. Then append them to the Document in their proper locations.
+//-----------------------------------------------------------------------------------------------------------------
+//
+// var sectionHTML = document.createElement('section');
+// var h1HTML = document.createElement('h1');
+// var ulHTML = document.createElement('ul');
+// var liHTML = document.createElement('li');
+//
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var firstAndPike = {
-  // MIN CUST= 23
-  //MAX CUST= 65
-  //AVG COOKIE= 6.3
+  name: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  sales: [],
+  calculator: function(i){
+    this.sales[i]= Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookies);
+    return this.sales[i];
+  },
+  renderHTML: function() {
+    var sectionHTML = document.createElement('section');
+    document.body.appendChild(sectionHTML);
+    var h1HTML = document.createElement('h1');
+    h1HTML.innerText = this.name;
+    sectionHTML.appendChild(h1HTML);
+    var ulHTML = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.calculator(i) + ' cookies';
+      ulHTML.appendChild(li);
+    }
+    var totalSales = 0;
+    for (i = 0; i < this.sales.length; i++) {
+      totalSales += this.sales[i];
+    }
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+    ulHTML.appendChild(li);
+    sectionHTML.appendChild(ulHTML);
+  }
 };
 var seaTacAirport = {
   // MIN CUST= 3
   //MAX CUST= 24
   //AVG COOKIE= 1.2
+  name: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2,
+  sales: [],
+  calculator: function(i){
+    this.sales[i]= Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookies);
+    return this.sales[i];
+  },
+  renderHTML: function() {
+    var sectionHTML = document.createElement('section');
+    document.body.appendChild(sectionHTML);
+    var h1HTML = document.createElement('h1');
+    h1HTML.innerText = this.name;
+    sectionHTML.appendChild(h1HTML);
+    var ulHTML = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.calculator(i) + ' cookies';
+      ulHTML.appendChild(li);
+    }
+    var totalSales = 0;
+    for (i = 0; i < this.sales.length; i++) {
+      totalSales += this.sales[i];
+    }
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+    ulHTML.appendChild(li);
+    sectionHTML.appendChild(ulHTML);
+  }
 };
+
 var seattleCenter = {
   // MIN CUST= 11
   //MAX CUST= 38
   //AVG COOKIE= 3.7
+  name: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7,
+  sales: [],
+  calculator: function(i){
+    this.sales[i]= Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookies);
+    return this.sales[i];
+  },
+  renderHTML: function() {
+    var sectionHTML = document.createElement('section');
+    document.body.appendChild(sectionHTML);
+    var h1HTML = document.createElement('h1');
+    h1HTML.innerText = this.name;
+    sectionHTML.appendChild(h1HTML);
+    var ulHTML = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.calculator(i) + ' cookies';
+      ulHTML.appendChild(li);
+    }
+    var totalSales = 0;
+    for (i = 0; i < this.sales.length; i++) {
+      totalSales += this.sales[i];
+    }
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+    ulHTML.appendChild(li);
+    sectionHTML.appendChild(ulHTML);
+  }
 };
+
 var capitolHill = {
   // MIN CUST= 20
   //MAX CUST= 38
   //AVG COOKIE= 2.3
+  name: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3,
+  sales: [],
+  calculator: function(i){
+    this.sales[i]= Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookies);
+    return this.sales[i];
+  },
+  renderHTML: function() {
+    var sectionHTML = document.createElement('section');
+    document.body.appendChild(sectionHTML);
+    var h1HTML = document.createElement('h1');
+    h1HTML.innerText = this.name;
+    sectionHTML.appendChild(h1HTML);
+    var ulHTML = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.calculator(i) + ' cookies';
+      ulHTML.appendChild(li);
+    }
+    var totalSales = 0;
+    for (i = 0; i < this.sales.length; i++) {
+      totalSales += this.sales[i];
+    }
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+    ulHTML.appendChild(li);
+    sectionHTML.appendChild(ulHTML);
+  }
 };
 var alki = {
   // MIN CUST= 2
   //MAX CUST= 16
   //AVG COOKIE= 4.6
+  name: 'alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  sales: [],
+  calculator: function(i){
+    this.sales[i]= Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookies);
+    return this.sales[i];
+  },
+  renderHTML: function() {
+    var sectionHTML = document.createElement('section');
+    document.body.appendChild(sectionHTML);
+    var h1HTML = document.createElement('h1');
+    h1HTML.innerText = this.name;
+    sectionHTML.appendChild(h1HTML);
+    var ulHTML = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.calculator(i) + ' cookies';
+      ulHTML.appendChild(li);
+    }
+    var totalSales = 0;
+    for (i = 0; i < this.sales.length; i++) {
+      totalSales += this.sales[i];
+    }
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+    ulHTML.appendChild(li);
+    sectionHTML.appendChild(ulHTML);
+  }
 };
+firstAndPike.calculator();
+firstAndPike.renderHTML();
+seaTacAirport.calculator();
+seaTacAirport.renderHTML();
+seattleCenter.calculator();
+seattleCenter.renderHTML();
+capitolHill.calculator();
+capitolHill.renderHTML();
+alki.calculator();
+alki.renderHTML();
